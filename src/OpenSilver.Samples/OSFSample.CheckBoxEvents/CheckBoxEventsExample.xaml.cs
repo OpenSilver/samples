0f743e80a60e0fd4ef345a0a28fb8c5137c91c91
Jaewung Lee <james@jamesnet.dev>
2025-03-19 12:03:50 +0800

    Updates

---
+++ src/OpenSilver.Samples/OSFSample.CheckBoxEvents/CheckBoxEventsExample.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Jamesnet.Foundation;
+using OSFSample.Support.UI.Units;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace OSFSample.CheckBoxEvents;
+
+public partial class CheckBoxEventsExample : ExampleBase
+{
+    public CheckBoxEventsExample()
+    {
+        this.InitializeComponent();
+    }
+    private void CheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+        txtStatus.Text = "Notifications enabled";
+        rectIndicator.Fill = new SolidColorBrush(Colors.Green);
+        MessageBox.Show("You will receive email notifications");
+    }
+    private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+        txtStatus.Text = "Notifications disabled";
+        rectIndicator.Fill = new SolidColorBrush(Colors.Red);
+        MessageBox.Show("Email notifications have been turned off");
+    }
+}
