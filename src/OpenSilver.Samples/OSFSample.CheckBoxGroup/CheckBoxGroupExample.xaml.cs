63cac94d9aa93787a4f3967bf275916c9895903c
Jaewung Lee <james@jamesnet.dev>
2025-03-19 14:24:57 +0800

    Updates

---
+++ src/OpenSilver.Samples/OSFSample.CheckBoxGroup/CheckBoxGroupExample.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Jamesnet.Foundation;
+using OSFSample.Support.UI.Units;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace OSFSample.CheckBoxGroup;
+
+public partial class CheckBoxGroupExample : ExampleBase
+{
+    public CheckBoxGroupExample()
+    {
+        this.InitializeComponent();
+        UpdateSummary();
+    }
+    private void NotificationOption_Changed(object sender, RoutedEventArgs e)
+    {
+        UpdateSummary();
+    }
+
+    private void UpdateSummary()
+    {
+        List<string> selectedOptions = new List<string>();
+
+        if (chkEmail.IsChecked == true) selectedOptions.Add("Email");
+        if (chkSMS.IsChecked == true) selectedOptions.Add("SMS");
+        if (chkPush.IsChecked == true) selectedOptions.Add("Push");
+        if (chkInApp.IsChecked == true) selectedOptions.Add("In-App");
+
+        if (selectedOptions.Count > 0)
+        {
+            txtSummary.Text = $"You will receive notifications via: {string.Join(", ", selectedOptions)}";
+            btnSave.IsEnabled = true;
+        }
+        else
+        {
+            txtSummary.Text = "No notification methods selected";
+            btnSave.IsEnabled = false;
+        }
+    }
+
+    private void BtnSave_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Notification preferences saved successfully!");
+    }
+}
---
+++ src/OpenSilver.Samples/OSFSample.CheckBoxThreeState/CheckBoxThreeStateExample.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Jamesnet.Foundation;
+using OSFSample.Support.UI.Units;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace OSFSample.CheckBoxThreeState;
+
+public partial class CheckBoxThreeStateExample : ExampleBase
+{
+    public CheckBoxThreeStateExample()
+    {
+        this.InitializeComponent();
+    }
+    private void ParentCheckBox_CheckedChanged(object sender, RoutedEventArgs e)
+    {
+        // Modify child checkboxes based on parent state
+        bool? isChecked = parentCheckBox.IsChecked;
+
+        childCheckBox1.IsChecked = isChecked;
+        childCheckBox2.IsChecked = isChecked;
+        childCheckBox3.IsChecked = isChecked;
+
+        UpdateStatus();
+    }
+    private void ChildCheckBox_CheckedChanged(object sender, RoutedEventArgs e)
+    {
+        // Update parent checkbox based on children
+        bool allChecked = (childCheckBox1.IsChecked == true) &&
+                         (childCheckBox2.IsChecked == true) &&
+                         (childCheckBox3.IsChecked == true);
+
+        bool allUnchecked = (childCheckBox1.IsChecked == false) &&
+                           (childCheckBox2.IsChecked == false) &&
+                           (childCheckBox3.IsChecked == false);
+
+        if (allChecked)
+        {
+            parentCheckBox.IsChecked = true;
+        }
+        else if (allUnchecked)
+        {
+            parentCheckBox.IsChecked = false;
+        }
+        else
+        {
+            parentCheckBox.IsChecked = null; // Indeterminate state
+        }
+
+        UpdateStatus();
+    }
+
+    private void UpdateStatus()
+    {
+        int checkedCount = 0;
+        if (childCheckBox1.IsChecked == true) checkedCount++;
+        if (childCheckBox2.IsChecked == true) checkedCount++;
+        if (childCheckBox3.IsChecked == true) checkedCount++;
+
+        txtStatus.Text = $"Selected items: {checkedCount} of 3";
+    }
+}
